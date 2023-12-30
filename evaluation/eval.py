c7d36713a6befc1a7808e37a75a180b84db9ce62
Omunkhuush <omunkhuush01@gmail.com>
2023-12-30 17:11:55 +0400

    eval.py

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -2,9 +2,9 @@ from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
 
-coco_gt = COCO('../../../evaluation/Fisheye1K_eval/groundtruth.json')
+coco_gt = COCO('groundtruth.json')
 coco_dt = coco_gt.loadRes(
-    '../../../evaluation/Fisheye1K_eval/detection_dgx.json')
+    'detection_dgx.json')
 
 print(type(coco_dt))
 print(type(coco_gt))

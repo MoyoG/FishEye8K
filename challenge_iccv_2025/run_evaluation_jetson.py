7e7832273b23351fb91a900aaef84fc34c5c82c6
Ganzorig <ganzobtn@gmail.com>
2025-06-06 22:24:31 +0800

    added time measurement and log into files

--- challenge_iccv_2025/run_evaluation_jetson.py
+++ challenge_iccv_2025/run_evaluation_jetson.py
@@ -35,18 +35,71 @@ def main():
 
     predictions = []
     print('Prediction started')
+    img_shapes= []
+    load_times = []
+    inference_times = []
+    postprocess_times = []
+    total_times = []
+    load_time = 0
+    inference_time = 0
+    postprocess_time = 0
+    total_time = 0
     start_time = time.time()
     for image_path in image_files:
+        t0=time.time()
         img = preprocess_image(image_path)
+        t1 = time.time()
+
         results = model(img,verbose=False)
+        t2 = time.time()
         results = postprocess_result(results) # [boxes, scores, classes]
+        t3 = time.time()
         predictions.append((image_path,results))
         #print(f"Processed {os.path.basename(image_path)}: {len(results[0])} objects detected.")
 
+
+        load_time += (t1-t0)
+        inference_time  += ( t2-t1)
+        postprocess_time += (t3-t2)
+        total_time += (t3-t0)
+        load_times.append(t1-t0)
+        inference_times.append(t2-t1)
+        postprocess_times.append(t3-t2)
+        total_times.append(t3-t0)
+        img_shapes.append(img.shape)
+
+        # print(f"image shape          : {img.shape}")
+        # print(f"Image Load Time      : {(t1 - t0)*1000:.2f} ms")
+        # print(f"Inference Time       : {(t2 - t1)*1000:.2f} ms")
+        # print(f"Postprocessing Time  : {(t3 - t2)*1000:.2f} ms")
+        # print(f"Total Time           : {(t3 - t0)*1000:.2f} ms")
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Processed {len(image_files)} images in {elapsed_time:.2f} seconds.")
 
+    print(f"Avg Image Load Time      : {load_time/len(image_files)*1000:.2f} ms")
+    print(f"Avg Inference Time       : {inference_time/len(image_files)*1000:.2f} ms")
+    print(f"Avg Postprocessing Time  : {postprocess_time/len(image_files)*1000:.2f} ms")
+    print(f"Avg Total Time           : {total_time/len(image_files)*1000:.2f} ms")
+
+    with open('/data/yolo11n_tensorrt_load_times.txt', 'w') as f:
+        for t in load_times:
+            f.write(f"{t}\n")
+
+    with open('/data/yolo11n_tensorrt_inference_times.txt', 'w') as f:
+        for t in inference_times:
+            f.write(f"{t}\n")
+
+    with open('/data/yolo11n_tensorrt_postprocess_times.txt', 'w') as f:
+        for t in postprocess_times:
+            f.write(f"{t}\n")
+
+    with open('/data/yolo11n_tensorrt_total_times.txt', 'w') as f:
+        for t in total_times:
+            f.write(f"{t}\n")
+    with open('/data/yolo11n_img_shapes.txt', 'w') as f:
+        for t in img_shapes:
+            f.write(f"{t}\n")
     predictions_json = []
     # Save predictions to JSON
     with open(args.output_json, 'w') as f:
